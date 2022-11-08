8c69b763accfbcc42d502cdd842468b95546e8da
Edher <edhcoronle000@gmail.com>
2022-11-08 16:11:49 -0300

    archivo fichas_pag.dart agg para la implementacion de fichas clinicas

---
+++ lib/pages/fichas_pag.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Fichas extends StatelessWidget {
+  const Fichas({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Fichas"),
+        )
+    );
+  }
+}

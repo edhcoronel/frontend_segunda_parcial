ff98b8eb6db77f354c38481fdcca7f5120ef6c43
Edher <edhcoronle000@gmail.com>
2022-11-14 03:49:35 -0300

    archivo para la implementacion de agregar fichas clinicas

---
+++ lib/pages/agg_fichas.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Agg_fichas extends StatefulWidget {
+  const Agg_fichas({Key? key}) : super(key: key);
+
+  @override
+  State<Agg_fichas> createState() => _Agg_fichasState();
+}
+
+class _Agg_fichasState extends State<Agg_fichas> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Agregar Fichas'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('No Se llego a Implementar'),
+        ),
+      ),
+    );
+  }
+}
